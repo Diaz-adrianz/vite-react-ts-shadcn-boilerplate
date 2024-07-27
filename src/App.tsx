9380e28c95e38dcbb6af505348455143dc9925b8
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-07-27 08:37:28 +0700

    feat: init toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { ThemeProvider } from './components/molecules/providers';
+import { Toast } from './components/atoms';
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light">
       <RouterProvider router={router} />
+      <Toast />
     </ThemeProvider>
   );
 }
